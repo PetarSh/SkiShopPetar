6b8c3a517df91e7cba218517331c3e2bbcffbf0e
Petar Shatanovski <p.shatanovski@gmail.com>
2021-09-16 13:33:03 +0200

    Sekcija 9

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,21 +1,15 @@
-
 import { Component, OnInit } from '@angular/core';
-
-
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit{
+export class AppComponent implements OnInit {
+  title = 'SkiNet';
 
-  title = 'Ski Shop Petar';
-  
-
-  constructor(){}
+  constructor() { }
 
   ngOnInit(): void {
-    
   }
 }
