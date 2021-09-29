795c7550603aead7d2411d3eaa690f033b9d3430
PetarSh <74772887+PetarSh@users.noreply.github.com>
2021-09-29 15:46:17 +0200

    Sekcija 18

--- API/Extensions/ApplicationServiceExtensions.cs
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -8,33 +8,35 @@ using Microsoft.Extensions.DependencyInjection;
 
 namespace API.Extensions
 {
-    public static class ApplicationServiceExtensions
+  public static class ApplicationServiceExtensions
+  {
+    public static IServiceCollection AddApplicationServices(this IServiceCollection services)
     {
-        public static IServiceCollection AddApplicationServices(this IServiceCollection services)
-        {
-            services.AddScoped<ITokenService, TokenService>();
-            services.AddScoped<IProductRepository, ProductRepository>();
-            services.AddScoped(typeof(IGenericRepository<>), (typeof(GenericRepository<>)));
-            services.AddScoped<IBasketRepository, BasketRepository>();
-            services.Configure<ApiBehaviorOptions>(options => 
+      services.AddScoped<ITokenService, TokenService>();
+      services.AddScoped<IProductRepository, ProductRepository>();
+      services.AddScoped<IUnitOfWork, UnitOfWork>();
+      services.AddScoped(typeof(IGenericRepository<>), (typeof(GenericRepository<>)));
+      services.AddScoped<IBasketRepository, BasketRepository>();
+      
+      services.Configure<ApiBehaviorOptions>(options =>
+      {
+        options.InvalidModelStateResponseFactory = actionContext =>
+              {
+            var errors = actionContext.ModelState
+                      .Where(e => e.Value.Errors.Count > 0)
+                      .SelectMany(x => x.Value.Errors)
+                      .Select(x => x.ErrorMessage).ToArray();
+
+            var errorResponse = new ApiValidationErrorResponse
             {
-                options.InvalidModelStateResponseFactory = actionContext => 
-                {
-                    var errors = actionContext.ModelState
-                        .Where(e => e.Value.Errors.Count > 0)
-                        .SelectMany(x => x.Value.Errors)
-                        .Select(x => x.ErrorMessage).ToArray();
+              Errors = errors
+            };
 
-                    var errorResponse = new ApiValidationErrorResponse
-                    {
-                        Errors = errors
-                    };
+            return new BadRequestObjectResult(errorResponse);
+          };
+      });
 
-                    return new BadRequestObjectResult(errorResponse);
-                };
-            });
-
-            return services;
-        }
+      return services;
     }
+  }
 }
--- Infrastructure/Data/GenericRepository.cs
+++ Infrastructure/Data/GenericRepository.cs
@@ -8,42 +8,60 @@ using Microsoft.EntityFrameworkCore;
 
 namespace Infrastructure.Data
 {
-    public class GenericRepository<T> : IGenericRepository<T> where T : BaseEntity
+  public class GenericRepository<T> : IGenericRepository<T> where T : BaseEntity
+  {
+    private readonly StoreContext _context;
+    public GenericRepository(StoreContext context)
     {
-        private readonly StoreContext _context;
-        public GenericRepository(StoreContext context)
-        {
-            _context = context;
-        }
-
-        public async Task<int> CountAsync(ISpecification<T> spec)
-        {
-            return await ApplySpecification(spec).CountAsync();
-        }
-
-        public async Task<T> GetByIdAsync(int id)
-        {
-            return await _context.Set<T>().FindAsync(id);
-        }
-
-        public async Task<T> GetEntityWithSpec(ISpecification<T> spec)
-        {
-            return await ApplySpecification(spec).FirstOrDefaultAsync();
-        }
-
-        public async Task<IReadOnlyList<T>> ListAllAsync()
-        {
-            return await _context.Set<T>().ToListAsync();
-        }
-
-        public async Task<IReadOnlyList<T>> ListAsync(ISpecification<T> spec)
-        {
-            return await ApplySpecification(spec).ToListAsync();
-        }
-
-        private IQueryable<T> ApplySpecification(ISpecification<T> spec)
-        {
-            return SpecificationEvaluator<T>.GetQuery(_context.Set<T>().AsQueryable(), spec);
-        }
+      _context = context;
     }
+
+    public void Add(T entity)
+    {
+      _context.Set<T>().Add(entity);
+    }
+
+    public async Task<int> CountAsync(ISpecification<T> spec)
+    {
+      return await ApplySpecification(spec).CountAsync();
+    }
+
+    public void Delete(T entity)
+    {
+      _context.Set<T>().Remove(entity);
+    }
+
+    public async Task<T> GetByIdAsync(int id)
+    {
+      return await _context.Set<T>().FindAsync(id);
+    }
+
+    public async Task<T> GetEntityWithSpec(ISpecification<T> spec)
+    {
+      return await ApplySpecification(spec).FirstOrDefaultAsync();
+    }
+
+    public async Task<IReadOnlyList<T>> ListAllAsync()
+    {
+      return await _context.Set<T>().ToListAsync();
+    }
+
+    public async Task<IReadOnlyList<T>> ListAsync(ISpecification<T> spec)
+    {
+      var query = ApplySpecification(spec);
+
+      return await query.ToListAsync();
+    }
+
+    public void Update(T entity)
+    {
+      _context.Set<T>().Attach(entity);
+      _context.Entry(entity).State = EntityState.Modified;
+    }
+
+    private IQueryable<T> ApplySpecification(ISpecification<T> spec)
+    {
+      return SpecificationEvaluator<T>.GetQuery(_context.Set<T>().AsQueryable(), spec);
+    }
+  }
 }
